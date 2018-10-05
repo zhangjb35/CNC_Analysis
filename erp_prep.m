d7758fe47f3318434e9072dbbc6af67ef981d668
Jinbo Zhang <zhangjb39@mail2.sysu.edu.cn>
2018-10-05 11:40:22 +0800

    import raw data

---
+++ erp_prep.m
@@ -0,0 +1,89 @@
+% SET PATHS
+
+clear variables
+restoredefaultpath; %% set a clean path
+home_dir = '/home/jinbo/Project/CNC/';
+
+matlab_dir = fullfile(home_dir, 'matlab'); % change according to your path
+data_dir = fullfile(home_dir, '/data/');
+erp_setup_path = fullfile(home_dir,'erp');
+figures_dir = []; % means no figures are saved
+
+% ADD PATHS
+
+% add your toolbox
+% addpath('/home/jinbo/.matlab/R2012b'); % for linux only
+addpath(fullfile(matlab_dir, 'fieldtrip-20180922'));
+ft_defaults %% initialize FieldTrip defaults
+addpath(fullfile(matlab_dir, 'eeglab14_1_2b'));
+addpath(genpath(fullfile(matlab_dir, 'FastICA_25/')));
+addpath(genpath(fullfile(matlab_dir, 'JinboToolbox/')));
+addpath(genpath(fullfile(matlab_dir, 'ERPWAVELABv1.2/')));
+
+eeglab
+eeglabpath = fileparts(which('eeglab')); % eeglab path
+
+bdfFile = fullfile(erp_setup_path,'bdf.txt');
+%% ERP
+[subjList,namePattern] = k_ls(fullfile(data_dir, 'sub-*', 'eeg', 'preprocess', 'coreog_ica_lfilt_hfilt_ref_data.set'));
+for i=1:length(subjList)
+    %% --- 01# prepare path
+    % setup store path
+    storePath=fullfile([filesep namePattern,sprintf('%02d',i)],'eeg','erp_70uv_threshodl');
+    mkdir(storePath);
+    %% --- 02# load to EEGLAB
+    EEG = pop_loadset('filename','coreog_ica_lfilt_hfilt_ref_data.set','filepath',fullfile([filesep namePattern,sprintf('%02d',i)],'eeg','preprocess'));
+    
+    %% --- 03# low pass filter 40 Hz
+    [warg, dev] = pop_kaiserbeta(0.0015);
+    m = pop_firwsord('kaiser', EEG.srate, 2, dev);
+    EEG = pop_firws(EEG, 'fcutoff', 40, 'ftype', 'lowpass', 'wtype', 'kaiser', 'warg', warg , 'forder', m, 'minphase', 0);
+    EEG.setname='eeg';
+    
+    %% --- 04# gen event list [ERPLAB]
+    EEG = pop_creabasiceventlist( EEG , 'AlphanumericCleaning', 'on', 'BoundaryNumeric', { -99 }, 'BoundaryString', { 'boundary' }, 'Eventlist',...
+        fullfile(storePath,'evenLst.txt'));
+    EEG.setname = 'event_eeg';
+    
+    %% --- 05# assign bin [ERPLAB]
+    EEG  = pop_binlister( EEG , 'BDF', bdfFile, 'IndexEL',  1, 'SendEL2', 'EEG', 'Voutput',...
+        'EEG' );
+    EEG.setname='ass_event_eeg';
+    pop_saveset(EEG, 'filename', EEG.setname, 'filepath', storePath);
+    
+    %% --- 06# extract epoch [ERPLAB]
+    EEG = pop_epochbin( EEG , [-200 800], 'pre');
+    EEG.setname='epoch_ass_event_eeg';
+    pop_saveset(EEG, 'filename', EEG.setname, 'filepath', storePath);
+    
+    %% --- 07# artifacts reject [ERPLAB]
+    EEG  = pop_artextval( EEG , 'Channel',  1:62, 'Flag', [ 1 2], 'Threshold', [ -70 70], 'Twindow', [-200 798] );
+    EEG.setname='artifact_epoch_ass_event_eeg';
+    EEG = pop_summary_AR_eeg_detection(EEG, fullfile(storePath, [EEG.setname, '.txt']));
+    pop_saveset(EEG, 'filename', EEG.setname, 'filepath', storePath);
+    
+    %% --- 08# average [ERPLAB]
+    ERP = pop_averager( EEG , 'Criterion', 'good', 'ExcludeBoundary', 'on', 'SEM', 'on' );
+    EEG.setname='avg_artifact_epoch_ass_event_eeg';
+    
+    %% --- 09# save ERP [ERPLAB]
+    ERP = pop_savemyerp(ERP, 'erpname',...
+        ['sub-' sprintf('%02d',i)], 'filename',  ['sub-' sprintf('%02d',i) '.erp'], 'filepath',storePath, 'Warning', 'off');
+    
+    % clear up
+    clear EEG
+end
+%% --- 10# grand avage  [ERPLAB]
+% get individual subject erp file list
+all_ERP = k_ls(fullfile(data_dir, 'sub-*', 'eeg', 'erp_70uv_threshodl', '*.erp'));
+dlmcell( 'grandAVG_list.txt',all_ERP);
+movefile('grandAVG_list.txt',[home_dir, filesep, 'erp']);
+% grand average and save grand erp
+ERP_weight = pop_gaverager(fullfile(home_dir, 'erp', 'grandAVG_list.txt') , 'Criterion',  30, 'ExcludeNullBin', 'on', 'SEM', 'on', 'Weighted', 'on');
+pop_savemyerp(ERP_weight, 'erpname',...
+       'grand_avg_weight', 'filename', 'grand_avg_weight.erp', 'filepath',fullfile(home_dir, 'erp') , 'Warning', 'off');
+   
+ERP_noweigth = pop_gaverager(fullfile(home_dir, 'erp', 'grandAVG_list.txt') , 'Criterion',  30, 'ExcludeNullBin', 'on', 'SEM', 'on', 'Weighted', 'off' );
+pop_savemyerp(ERP_noweigth, 'erpname',...
+       'grand_avg_noweight', 'filename', 'grand_avg_noweight.erp', 'filepath',fullfile(home_dir, 'erp') , 'Warning', 'off');
+%% --- 11# warning 30% limit, rm redo
---
+++ raw_eeg.m
@@ -0,0 +1,93 @@
+clear variables
+clc
+restoredefaultpath; %% set a clean path
+
+% path
+project_dir = pwd; % the file located
+home_dir = fullfile(project_dir, 'data', 'rawEEG');
+matlab_dir = fullfile(project_dir, 'toolbox');
+fuction_dir = fullfile(project_dir, 'functions');
+% toolbox
+% addpath('/home/jinbo/.matlab/R2012b');
+addpath(fullfile(matlab_dir, 'fieldtrip-20180922')); ft_defaults %% initialize FieldTrip defaults
+addpath(genpath([matlab_dir, filesep, 'eeglab14_1_2b']));
+addpath(genpath(fuction_dir));
+
+% located setup file from eeglab
+eeglabpath = fileparts(which('eeglab')); % eeglab path
+
+%% Preprocess
+[subjList,namePattern] = kb_ls(fullfile(home_dir,'sub-*','eeg','*.set'));
+for i=2:length(subjList)
+    %% --- 01# prepare path
+    % setup store path
+    storePath=fullfile([filesep namePattern,sprintf('%02d',i)],'eeg','preprocess');
+    mkdir(storePath);
+    %% --- 02# load to EEGLAB
+    EEG = pop_loadset('filename',['sub-' sprintf('%02d',i) '_task-psychophysics_eeg.set'],'filepath',fullfile([filesep namePattern,sprintf('%02d',i)],'eeg'));
+    
+    %% --- 03# remove empty channel
+    EEG = pop_select( EEG,'nochannel',{'M1' 'EKG' 'EMG'});
+    
+    %% --- 04# load location
+    EEG=pop_chanedit(EEG, 'lookup',...
+        [eeglabpath '' filesep 'plugins' filesep 'dipfit2.3' filesep 'standard_BESA' filesep 'standard-10-5-cap385.elp'],...
+        'changefield',{63 'labels' 'AF8'},'changefield',{59 'labels' 'AF7'},...
+        'changefield',{57 'labels' 'PO10'},'changefield',{53 'labels' 'PO9'},'lookup',...
+        [eeglabpath '' filesep 'plugins' filesep 'dipfit2.3' filesep 'standard_BESA' filesep 'standard-10-5-cap385.elp']);
+    EEG.setname='origin_data';
+    
+    %% --- 05# calc semi M2 with erplab
+    EEG = pop_eegchanoperator( EEG,...
+        { 'ch66 = ch42/2.0 label semiM2'} , ...
+        'ErrorMsg', 'popup', 'Warning', 'off' );
+    
+    %% --- 06# re-ref
+    EEG = pop_reref( EEG, 66,'exclude',[64 65] );
+    
+    %% --- 07# remove m2 from dataset
+    EEG = pop_select( EEG,'nochannel',{'M2'});
+    EEG.setname='ref_data';
+    
+    %% --- 08# high pass filter .5 Hz
+    [warg, dev] = pop_kaiserbeta(0.0015);
+    m = pop_firwsord('kaiser', EEG.srate, 2, dev);
+    EEG = pop_firws(EEG, 'fcutoff', 0.5, 'ftype', 'highpass', 'wtype', 'kaiser', 'warg', warg , 'forder', m, 'minphase', 0);
+    EEG.setname='hfilt_ref_data';
+    
+    %% --- 09# run ICA on continue data and correct EOG
+    % save data
+    EEG = pop_runica(EEG, 'icatype','runica','chanind',1:62);
+    EEG.setname='ica_lfilt_hfilt_ref_data';
+    pop_saveset(EEG, 'filename', EEG.setname, 'filepath', storePath);
+    
+    %% --- 10# correct eog
+    % save data
+    load([fullfile(fuction_dir) 'saica_cfg.mat'])
+    [EEG_temp, ~] = eeg_SASICA(EEG,varargin{1,1});
+    rmIndex = find(EEG_temp.reject.gcompreject>0);
+    
+    if ~isempty(rmIndex)
+        EEG = pop_subcomp( EEG_temp, rmIndex, 0);
+        clear EEG_temp
+        EEG.setname='coreog_ica_lfilt_hfilt_ref_data';
+        pop_saveset(EEG, 'filename', EEG.setname, 'filepath', storePath);
+    else
+        clear EEG_temp
+        clear varargin
+        load([fullfile(matlab_dir, 'JinboToolbox/') 'saica_cfg_adjust.mat']);
+        [EEG_temp, ~] = eeg_SASICA(EEG,varargin{1,1});
+        rmIndex = find(EEG_temp.reject.gcompreject>0);
+        if isempty(rmIndex)
+            clear EEG_temp
+            EEG.setname='failed_coreog_ica_lfilt_hfilt_ref_data';
+            pop_saveset(EEG, 'filename', EEG.setname, 'filepath', storePath);
+        else
+            EEG = pop_subcomp( EEG_temp, rmIndex, 0);
+            clear EEG_temp
+            EEG.setname='coreog_ica_lfilt_hfilt_ref_data';
+            pop_saveset(EEG, 'filename', EEG.setname, 'filepath', storePath);
+        end
+    end
+    clear EEG
+end
