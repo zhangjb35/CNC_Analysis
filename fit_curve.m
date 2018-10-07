f5d5c33785d9a8e0f073f860e9c3b73447a2fdfa
Jinbo Zhang <zhangjb39@mail2.sysu.edu.cn>
2018-10-07 20:42:08 +0800

    行为结果确定

--- fit_curve.m
+++ fit_curve.m
@@ -48,19 +48,19 @@ addpath(genpath(fuction_dir));
 %% application
 %% prep data
 %
-data_file_behv = cell2mat(kb_ls(fullfile(pwd,'data','behv','*.csv')));
+data_file_behv = cell2mat(kb_ls(fullfile(pwd,'data','behv','raw','sub*.csv')));
 rawData = readtable(data_file_behv);
 %% prep data for each subject
 rawData.ButtonPush = zeros(length(rawData.participant_id),1); % 0 = less;1=more
 % Determin Button Pushed
 for i=1:length(rawData.participant_id)
-    if rawData.NumCondition(i)<=3 && rawData.ACC(i)==1 % small num with right resp = less button
+    if rawData.NumCondition(i)<=3 && rawData.ACC(i)==1 % small num with right resp = less button; acc = 1 mean right response
         rawData.ButtonPush(i)=0;
     elseif rawData.NumCondition(i)<=3 && rawData.ACC(i)==0 % small num with wrong resp = more button
         rawData.ButtonPush(i)=1;
     elseif rawData.NumCondition(i)>3 &&rawData.ACC(i)==1 % bigger num with right resp = more button
         rawData.ButtonPush(i)=1;
-    elseif rawData.NumCondition(i)>3 && rawData.ACC(i)~=0 % bigger num with wrong resp = less button
+    elseif rawData.NumCondition(i)>3 && rawData.ACC(i)==0 % bigger num with wrong resp = less button
         rawData.ButtonPush(i)=0;
     end
 end
@@ -71,7 +71,7 @@ for i=1:length(unique(rawData.participant_id))
     xSets(:,i) = [15;16;18;22;24;27];% stimulus levels
     mSets(:,i) = repmat(20,size(xSets(:,i))); %
 end
-rSets = reshape(rightSet,[6,5,23]);
+rSets = reshape(rightSet,[6,5,22]);
 
 %% plot at single subject and condition level
 conditionName = {'No Sound','One Soft','One Loud','Multi Soft','Multi Loud'};
@@ -83,27 +83,46 @@ C2 = rgb('OrangeRed');
 C3 = rgb('OrangeRed');
 C4 = rgb('MediumAquamarine');
 C5 = rgb('MediumAquamarine');
-ColorUse = {C1,C2,C3,C4,C5};
-for i=1:23
+%ColorUse = {C1,C2,C3,C4,C5};
+ColorUse = {'k','r','r','b','b'};
+for i=1:22
     fig=figure;
     for j=1:5
         x = xSets(:,i);
         m = mSets(:,i);
         r = rSets(:,j,i);
-        plot( x, r ./ m, 'Marker',markShapeUse{j},'MarkerFaceColor', 'none','MarkerEdgeColor',ColorUse{j}, 'LineStyle' ,'none','MarkerSize',5,'DisplayName', conditionNameMarker{j}); 
+        semilogx( x, r ./ m, 'Marker',markShapeUse{j},'MarkerFaceColor', 'none','MarkerEdgeColor',ColorUse{j}, 'LineStyle' ,'none','MarkerSize',5,'DisplayName', conditionNameMarker{j});
         axis([14.5 27.5 0 1]);
         axis square;
         %% For the Gaussian cumulative distribution function (black curve):
         degpol = 1; % Degree of the polynomial
         guessing = 1/2; % guessing rate
-        lapsing = 0; % lapsing rate
+        % lapsing = 0; % lapsing rate
+        
         b = binomfit_lims( r, m, x, degpol, 'probit' );
         numxfit = 199; % Number of new points to be generated minus 1
         xfit = [min(x):(max(x)-min(x))/numxfit:max( x ) ]';
         % Plot the fitted curve
         pfit = binomval_lims( b, xfit, 'probit' );
-        hold on, plot( xfit, pfit, 'LineStyle' ,lineUse{j},'Color',ColorUse{j},'LineWidth',2,'DisplayName',conditionName{j});
-        legend('Location','southeast')
+        prob = 0.25; % probability where to estimate the PSE
+        [ X_25_ind(i,j), ~ ] = threshold_slope( pfit, xfit, prob);
+        prob = 0.5; % probability where to estimate the PSE
+        [ X_50_ind(i,j), ~ ] = threshold_slope( pfit, xfit, prob);
+        prob = 0.75; % probability where to estimate the JND
+        [ X_75_ind(i,j), ~ ] = threshold_slope( pfit, xfit, prob );
+        hold on, semilogx( xfit, pfit, 'LineStyle' ,lineUse{j},'Color',ColorUse{j},'LineWidth',2,'DisplayName',conditionName{j});
+%     %% For the Weibull function (red curve):
+%     [ b, K ] = binom_weib( r, m, x, 1, 5 );
+%     guessing = 0; % guessing rate
+%     lapsing = 0; % lapsing rate
+%     % Plot the fitted curve
+%     pfit = binomval_lims( b, xfit, 'weibull', guessing, lapsing, K );
+%     prob_pse = 0.5; % probability where to estimate the PSE
+%     [ PSE_ind(i,j), ~ ] = threshold_slope( pfit, xfit, prob_pse );
+%     prob_jnd = 0.75; % probability where to estimate the JND
+%     [ JND_ind(i,j), ~ ] = threshold_slope( pfit, xfit, prob_jnd );
+%     hold on, semilogx( xfit, pfit, 'LineStyle' ,lineUse{j},'Color',ColorUse{j},'LineWidth',2,'DisplayName',conditionName{j});
+    legend('Location','southeast')
     end
     xticks([15;16;18;20;22;24;27])
     %xticklabels({'-3\pi','-2\pi','-\pi','0','\pi','2\pi','3\pi'})
@@ -125,9 +144,9 @@ end
 %     xfit = [min(x):(max(x)-min(x))/numxfit:max( x ) ]';
 %     pfit = locglmfit( xfit, r, m, x, bwd );
 %     hold on, plot( xfit, pfit, 'b' );
-%     
+%
 % %%  For the Weibull function (red curve):
-% 
+%
 % [ b, K ] = binom_weib( r, m, x );
 % guessing = 0; % guessing rate
 % lapsing = 0; % lapsing rate
@@ -137,3 +156,115 @@ end
 
 %% Extract PSE and JND
 %% Grand Average
+[GrightSet,GnumerlSet,GgroupName] = grpstats(rawData.ButtonPush,{rawData.SoundCondition,rawData.NumCondition},{'sum','numel','gname'});
+GrSets = reshape(GrightSet,[6,5]);
+Gx =  [15;16;18;22;24;27];
+Gm =  [440;440;440;440;440;440];
+% plot
+close all
+fig=figure;
+for j=1:5
+    x = Gx;
+    m = Gm;
+    r = GrSets(:,j);
+    semilogx( x, r ./ m, 'Marker',markShapeUse{j},'MarkerFaceColor', 'none','MarkerEdgeColor',ColorUse{j}, 'LineStyle' ,'none','MarkerSize',5,'DisplayName', conditionNameMarker{j});
+    axis([14.5 27.5 0 1]);
+    axis square;
+            %% For the Gaussian cumulative distribution function (black curve):
+            degpol = 1; % Degree of the polynomial
+            guessing = 1/2; % guessing rate
+            %lapsing = 100; % lapsing rate
+            b = binomfit_lims( r, m, x, degpol, 'probit' );
+            numxfit = 199; % Number of new points to be generated minus 1
+            xfit = [min(x):(max(x)-min(x))/numxfit:max( x ) ]';
+            % Plot the fitted curve
+            pfit = binomval_lims( b, xfit, 'probit' );
+            prob = 0.5; % probability where to estimate the PSE
+            [ Point_50(j), ~ ] = threshold_slope( pfit, xfit, prob );
+            prob = 0.75; % probability where to estimate the JND
+            [ Point_75(j), ~ ] = threshold_slope( pfit, xfit, prob );
+            prob = 0.25; % probability where to estimate the JND
+            [ Point_25(j), ~ ] = threshold_slope( pfit, xfit, prob );
+            hold on, semilogx( xfit, pfit, 'LineStyle' ,lineUse{j},'Color',ColorUse{j},'LineWidth',1,'DisplayName',conditionName{j});
+
+%     
+%         bwd_min = min( diff( x ) );
+%         bwd_max = max( x ) - min( x );
+%         bwd = bandwidth_cross_validation( r, m, x, [ bwd_min, bwd_max ] );
+%         % Plot the fitted curve
+%         bwd = bwd(3); % choose the third estimate, which is based on cross-validated deviance
+%         pfit = locglmfit( xfit, r, m, x, bwd );
+%         hold on, plot( xfit, pfit, 'LineStyle' ,lineUse{j},'Color',ColorUse{j},'LineWidth',2,'DisplayName',conditionName{j});
+    %% For the Weibull function (red curve):
+%     [ b, K ] = binom_weib( r, m, x, 1, 5 );
+%     guessing = 0; % guessing rate
+%     lapsing = 0; % lapsing rate
+%     % Plot the fitted curve
+%     pfit = binomval_lims( b, xfit, 'weibull', guessing, lapsing, K );
+%     prob_pse = 0.5; % probability where to estimate the PSE
+%     [ PSE(j), ~ ] = threshold_slope( pfit, xfit, prob_pse );
+%     prob_jnd = 0.75; % probability where to estimate the JND
+%     [ JND(j), ~ ] = threshold_slope( pfit, xfit, prob_jnd );
+%     hold on, semilogx( xfit, pfit, 'LineStyle' ,lineUse{j},'Color',ColorUse{j},'LineWidth',2,'DisplayName',conditionName{j});
+    
+    legend('Location','southeast')
+end
+xticks([15;16;18;20;22;24;27])
+%xticklabels({'-3\pi','-2\pi','-\pi','0','\pi','2\pi','3\pi'})
+yticks([0 0.25 0.50 0.75 1])
+title('Grand Psychometric Curves', 'FontSize', 18);
+xlabel('Test numerosity (dots)','FontSize', 18) % x-axis label
+ylabel('Proportion of "more elements"','FontSize', 18) % y-axis label
+print(fig,'Grand_Curve','-dpng','-r300')
+hold off
+close all
+%% write out
+% for jasp
+pse_data = array2table(X_50_ind);
+pse_data.Properties.VariableNames={'No_Sound','One_Soft','One_Loud','Multi_Soft','Multi_Loud'};
+writetable(pse_data,'pse_results.csv','WriteVariableNames',true)
+jnd_data_25_75 = array2table((X_75_ind-X_25_ind)/2);
+jnd_data_25_75.Properties.VariableNames={'No_Sound','One_Soft','One_Loud','Multi_Soft','Multi_Loud'};
+writetable(jnd_data_25_75,'jnd_results_25_75.csv','WriteVariableNames',true)
+jnd_data_75_50 = array2table(X_75_ind-X_50_ind);
+jnd_data_75_50.Properties.VariableNames={'No_Sound','One_Soft','One_Loud','Multi_Soft','Multi_Loud'};
+writetable(jnd_data_75_50,'jnd_results_75_50.csv','WriteVariableNames',true)
+% mat
+save X_25_ind X_25_ind
+save X_50_ind X_50_ind
+save X_75_ind X_75_ind
+%% screen
+% pse
+limitUP = mean(table2array(pse_data))+2.5*(std(table2array(pse_data)));
+limitDown = mean(table2array(pse_data))-2.5*(std(table2array(pse_data)));
+temp=table2array(pse_data);
+for i=1:size(temp,2)
+    checkData = temp(:,i);
+    checkData(find(checkData<limitDown(i)))=nan;
+    checkData(find(checkData>limitUP(i)))=nan;
+    checkEd(:,i)=checkData;
+end
+checkEd = array2table(checkEd);
+checkEd.Properties.VariableNames={'No_Sound','One_Soft','One_Loud','Multi_Soft','Multi_Loud'};
+writetable(checkEd,'pse_results_checked.csv','WriteVariableNames',true)
+clear limitUP
+clear limitDown
+clear checkData
+clear checkEd
+% jnd
+limitUP = mean(table2array(jnd_data_75_50))+2.5*(std(table2array(jnd_data_75_50)));
+limitDown = mean(table2array(jnd_data_75_50))-2.5*(std(table2array(jnd_data_75_50)));
+temp=table2array(jnd_data_75_50);
+for i=1:size(temp,2)
+    checkData = temp(:,i);
+    checkData(find(checkData<limitDown(i)))=nan;
+    checkData(find(checkData>limitUP(i)))=nan;
+    checkEd(:,i)=checkData;
+end
+checkEd = array2table(checkEd);
+checkEd.Properties.VariableNames={'No_Sound','One_Soft','One_Loud','Multi_Soft','Multi_Loud'};
+writetable(checkEd,'jnd_results_checked.csv','WriteVariableNames',true)
+%%
+save Point_25 Point_25
+save Point_50 Point_50
+save Point_75 Point_75
